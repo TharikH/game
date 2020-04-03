db5d2f426a0119c704c6c3cc38569c3076320779
Tharikh <tharikhlegend@gmail.com>
2020-04-03 18:54:04 +0530

    game dev starting phase

---
+++ game.py
@@ -0,0 +1,20 @@
+import pygame,os,sys
+from pygame.locals import *
+
+red= (255,0,0)
+
+pygame.init()
+
+window=pygame.display.set_mode((1000,600))
+pygame.display.set_caption('Slither.eat -The Snake Game')
+
+screen=pygame.display.get_surface()
+screen.fill(red)
+pygame.display.set_caption("Snake")
+pygame.display.flip()
+
+count=0
+
+while True:
+    print("Slither.eat-Snake game!")
+    pass
