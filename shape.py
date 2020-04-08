24e8a68114bf2c9e95ae9f4d3ad3d01dc66ee0c4
Tharikh <tharikhlegend@gmail.com>
2020-04-08 17:43:36 +0530

    shapes created

---
+++ shape.py
@@ -0,0 +1,38 @@
+import pygame,sys
+import math
+from pygame.locals import *
+
+pygame.init()
+
+DISPLAYSURF=pygame.display.set_mode((400,300),0,32)
+pygame.display.set_caption('Drawing')
+
+BLACK=(0,0,0)
+WHITE=(255,255,255)
+RED=(255,0,0)
+BLUE=(0,0,255)
+GREEN=(0,255,0)
+
+DISPLAYSURF.fill(WHITE)
+pygame.draw.polygon(DISPLAYSURF,GREEN,[(150+100*math.cos(2*math.pi*i/5),150+100*math.sin(2*math.pi*i/5))for i in range(5)])
+pygame.draw.line(DISPLAYSURF,BLUE,(60,60),(120,60),4)
+pygame.draw.line(DISPLAYSURF,BLUE,(120,60),(60,120))
+pygame.draw.line(DISPLAYSURF,BLUE,(60,120),(120,120),4)
+pygame.draw.circle(DISPLAYSURF,BLUE,(300,50),20,0)
+pygame.draw.ellipse(DISPLAYSURF,RED,(300,200,40,80),1)
+pygame.draw.rect(DISPLAYSURF,RED,(200,150,100,50))
+
+pixObj=pygame.PixelArray(DISPLAYSURF)
+pixObj[380][280]=BLACK
+pixObj[382][282]=BLACK
+pixObj[384][284]=BLACK
+pixObj[386][286]=BLACK
+pixObj[388][288]=BLACK
+del pixObj
+
+while True:
+    for event in pygame.event.get():
+        if(event.type==QUIT):
+            pygame.quit()
+            sys.exit()
+    pygame.display.update()
